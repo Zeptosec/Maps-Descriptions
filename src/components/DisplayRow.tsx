b9a8b02bee7ff028f8e6f4695403f96153e05b5e
Zeptosec <armandasmatkevicius@gmail.com>
2023-01-20 15:15:39 +0200

    Made link open in new tab

--- src/components/DisplayRow.tsx
+++ src/components/DisplayRow.tsx
@@ -12,7 +12,7 @@ export default function DisplayRow({ desc, startEdit, remove, user }: RowData) {
 
     return (
         <>
-            <td className=" align-top"><a className="text-blue-600" href={desc.link}>{desc.name}</a></td>
+            <td className=" align-top"><a className="text-blue-600" target="_blank" href={desc.link}>{desc.name}</a></td>
             <td className=" align-top"><div dangerouslySetInnerHTML={{ __html: desc.size }} /></td>
             <td className=" align-top"><div dangerouslySetInnerHTML={{ __html: desc.description }} /></td>
             <td className="w-32 align-top">{desc.time.map(w => new Date(w.seconds * 1000).toISOString().split('T')[0]).join(", ")}</td>
